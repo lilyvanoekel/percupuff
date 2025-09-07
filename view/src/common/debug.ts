e0ee1e0ef206794aa9fe9ba72d9a9fe84d8ff061
Lily van Oekel <lilyvanoekel@gmail.com>
2025-09-07 18:29:05 -0400

    explain nuclearDebug

--- view/src/common/debug.ts
+++ view/src/common/debug.ts
@@ -1,3 +1,4 @@
+// It can be hard to debug without a console log when using Cmajor. This will write values directly into the body instead.
 export const nuclearDebug = (x: unknown) => {
   document.body.innerHTML =
     '<div style="height:400px; overflow: scroll;"><pre style="font-size: 24px; background: black; color: lime; padding: 20px;">DEBUG: ' +
