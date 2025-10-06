0765666debb540956cf46abab7d232e201b025bb
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2025-10-07 03:28:05 +0530

    Add Ride Bell instrument and integrate into synthesizer

--- view/src/params.ts
+++ view/src/params.ts
@@ -32,7 +32,7 @@ export const instruments = {
 
   cowbell: { name: "Cowbell", group: "🐮", midi: 56 },
 
-  rideBell: { name: "Ride Bell", group: "Hihats", midi: 53 },
+  rideBell: { name: "Ride Bell", group: null, midi: 53 },
 
   // Example of adding an instrument without having it show up in the UI yet.
   sideStick: { name: "Side Stick", group: null, midi: 37 },
