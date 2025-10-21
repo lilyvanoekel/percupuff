efd9603114746eb64de505bc1cdaa1fd869f0fda
Riley Chou <riley33chou@gmail.com>
2025-10-20 22:46:29 -0700

    first draft of side stick

--- view/src/params.ts
+++ view/src/params.ts
@@ -4,9 +4,11 @@ export const instruments = {
   kick1: { name: "Acoustic Kick", group: "Kicks", midi: 35 },
   kick2: { name: "Electric Kick", group: "Kicks", midi: 36 },
 
+  sideStick: { name: "Side Stick", group: "Snares", midi: 37 },
   snare1: { name: "Acoustic Snare", group: "Snares", midi: 38 },
   snare2: { name: "Electric Snare", group: "Snares", midi: 40 },
   clap1: { name: "Clap", group: "Snares", midi: 39 },
+
 
   hihat1: { name: "Closed Hihat", group: "Hihats", midi: 42 },
   hihat2: { name: "Pedal Hihat", group: "Hihats", midi: 44 },
@@ -35,7 +37,7 @@ export const instruments = {
   cowbell: { name: "Cowbell", group: "🐮", midi: 56 },
 
   // Example of adding an instrument without having it show up in the UI yet.
-  sideStick: { name: "Side Stick", group: null, midi: 37 },
+  // sideStick: { name: "Side Stick", group: null, midi: 37 },
 } as const;
 
 export const instrumentGroups = Array.from(
